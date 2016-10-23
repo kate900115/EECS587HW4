df112bc1f4e03b8cb8e66dec455ac30503787d71
kate900115 <kate900115@gmail.com>
2016-10-22 20:11:50 -0400

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,70 @@
 #include <iostream>
 #include <cmath>
-#include <omp>
+#include <omp.h>
+#include <queue>
 
-#define a 1
-#define b 100
+#define a 1.0
+#define b 100.0
 #define s 12
+#define e 1.0e(-10)
 
 using namespace std;
 
 double f (double x);
 
+
+
 int main()
 {
+
+	struct interval
+	{
+		double start;
+		double end;
+		double max;
+	};
+	queue <interval> works;
+	double fa = f(a);
+	double fb = f(b);
+	double max = fa>fb? fa:fb;
+	interval setup(a, b, max);
+	works.push(setup);
+	
+	double start1;
+	double end1;
+	double max1;
+	double start2;
+	double end2;
+	double max2;
+	
+	double interval_length;
+	
+	while 
+	{
+		interval old = works.pop();
+		double fstart = f(old.start);
+		double fmid = f((old.end+old.start)/2);
+		double fend = f(old.end);
+		if (fstart + fmid + s*(end-start)/4>old.max)
+		{
+			interval new1(old.start, (old.end+old.start)/2, fstart > fmid? fstart : fmid);
+			works.push(new1);
+		}
+		
+		if (fmid + fend + s*(end-start)/4>old.max)
+		{
+			interval new2((old.end+old.start)/2, old.end, fmid > fend? fmid : fend);
+			works.push(new2);
+		}
+		
+		interval_max new_interval(start,end,max);
+		works.push(new_interval);
+		
+	}
 	return 0;
 }
 
+//should not change
 double f(double x)
 {
 	double sumj=0;
